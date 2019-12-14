e70fd204bc2c0015dfa9f0a698cc7f9263e8b324
Davor Ocelic <docelic@crystallabs.io>
2019-12-14 02:43:27 +0100

    Improve example

--- examples/keys.cr
+++ examples/keys.cr
@@ -2,9 +2,6 @@ require "../src/term_app"
 
 pr = ::TermApp::Data.new
 
-pr.tput.clear
-pr.tput.move 10, 2
-
 #pr.input.on(::TermApp::DataEvent) { |e|
 #  pr.tput.print e.data[...e.len]
 #  #pr.tput.sety 10
@@ -14,9 +11,9 @@ pr.tput.move 10, 2
 #  pr.tput.move 10, 2
 #}
 
-pr.on(::TermApp::KeyPressEvent) { |e|
+pr.input.on(::TermApp::KeyPressEvent) { |e|
   puts e.key
-  pr.tput.move 10, 2
+  pr.tput.setx 0
 }
 
-sleep 120
+sleep 10
